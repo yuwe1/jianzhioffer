c63b466fe793f0f6aff340f403f1501ce493dd2c
yuwei1 <yuweijiubayiqi@163.com>
2020-01-14 16:03:56 +0800

    反转链表

---
+++ day03/demo6/README.md
@@ -0,0 +1,8 @@
+## 反转链表
+定义一个函数，输入一个链表的头结点，反转该链表并输出反转后链表的头结点。链表结点定义如下：
+```go
+type ListNode struct{
+    m_nkey int
+    m_pNext *ListNode
+}
+```
---
+++ day03/demo6/demo6.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	m_nkey  int
+	n_pNext *Node
+}
+type ListNode struct {
+	head   *Node
+	length int
+}
+
+func NewList() *ListNode {
+	return &ListNode{
+		nil, 0,
+	}
+}
+
+// 添加一个结点
+func (l *ListNode) addNode(node Node) {
+	if l.length == 0 {
+		l.head = &node
+	} else {
+		p := l.head
+		for p.n_pNext != nil {
+			p = p.n_pNext
+		}
+		p.n_pNext = &node
+	}
+	l.length++
+}
+func (l *ListNode) ReadNode() {
+	p := l.head
+	for p != nil {
+		fmt.Println(p.m_nkey)
+		p = p.n_pNext
+	}
+}
+
+// 反转链表
+func (l *ListNode) reverselist() {
+
+	var pre *Node
+	p := l.head
+	for p != nil {
+		temp := p.n_pNext
+		if temp == nil {
+			l.head = p
+		}
+		p.n_pNext = pre
+		pre = p
+		p = temp
+	}
+}
+func main() {
+	l := NewList()
+	l.addNode(Node{
+		1, nil,
+	})
+	l.addNode(Node{
+		2, nil,
+	})
+	l.ReadNode()
+	l.reverselist()
+	l.ReadNode()
+}
